7c019b870b8cef32da9eba6188f5468124f05f3f
Bazyli Cyran <bazyli@cyran.dev>
2024-06-11 19:38:38 +0000

    refactor(alacritty): Correctly use base16 colors

--- home/features/alacritty.nix
+++ home/features/alacritty.nix
@@ -4,28 +4,28 @@
     settings = {
       colors = {
         primary = {
-          background = "#${config.colorScheme.palette.bg}";
-          foreground = "#${config.colorScheme.palette.fg}";
+          background = "#${config.colorScheme.palette.base00}";
+          foreground = "#${config.colorScheme.palette.base05}";
         };
         normal = {
           black = "#${config.colorScheme.palette.base00}";
-          red = "#${config.colorScheme.palette.base01}";
-          green = "#${config.colorScheme.palette.base02}";
-          yellow = "#${config.colorScheme.palette.base03}";
-          blue = "#${config.colorScheme.palette.base04}";
-          magenta = "#${config.colorScheme.palette.base05}";
-          cyan = "#${config.colorScheme.palette.base06}";
-          white = "#${config.colorScheme.palette.base07}";
+          red = "#${config.colorScheme.palette.base08}";
+          green = "#${config.colorScheme.palette.base0B}";
+          yellow = "#${config.colorScheme.palette.base0A}";
+          blue = "#${config.colorScheme.palette.base0D}";
+          magenta = "#${config.colorScheme.palette.base0E}";
+          cyan = "#${config.colorScheme.palette.base0C}";
+          white = "#${config.colorScheme.palette.base05}";
         };
         bright = {
-          black = "#${config.colorScheme.palette.base08}";
-          red = "#${config.colorScheme.palette.base09}";
-          green = "#${config.colorScheme.palette.base0A}";
-          yellow = "#${config.colorScheme.palette.base0B}";
-          blue = "#${config.colorScheme.palette.base0C}";
-          magenta = "#${config.colorScheme.palette.base0D}";
-          cyan = "#${config.colorScheme.palette.base0E}";
-          white = "#${config.colorScheme.palette.base0F}";
+          black = "#${config.colorScheme.palette.base00}";
+          red = "#${config.colorScheme.palette.base08}";
+          green = "#${config.colorScheme.palette.base0B}";
+          yellow = "#${config.colorScheme.palette.base0A}";
+          blue = "#${config.colorScheme.palette.base0D}";
+          magenta = "#${config.colorScheme.palette.base0E}";
+          cyan = "#${config.colorScheme.palette.base0C}";
+          white = "#${config.colorScheme.palette.base05}";
         };
       };
       env = {
