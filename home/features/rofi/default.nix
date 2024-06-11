9ef34b7c3f167787beee2028be22a2418c9cbe5f
Bazyli Cyran <bazyli@cyran.dev>
2024-06-11 20:24:46 +0000

    refactor(rofi): Correctly use base16 colors

--- home/features/rofi/default.nix
+++ home/features/rofi/default.nix
@@ -3,19 +3,11 @@
   config,
   ...
 }: let
-  commonConfigPath = pkgs.substituteAll {
-    name = "common.rasi";
-    src = ./files/common.rasi;
-    colorBg = "#${config.colorScheme.palette.guiBg}";
-    colorBgDark = "#${config.colorScheme.palette.guiBgDark}";
-    colorBgHighlight = "#${config.colorScheme.palette.guiBgHighlight}";
-    colorFg = "#${config.colorScheme.palette.guiFg}";
-    colorFgDark = "#${config.colorScheme.palette.guiFgDark}";
-    colorAccentPrimary = "#${config.colorScheme.palette.guiAccentPrimary}";
-    colorAccentSecondary = "#${config.colorScheme.palette.guiAccentSecondary}";
-    colorWarning = "#${config.colorScheme.palette.guiWarning}";
-    colorError = "#${config.colorScheme.palette.guiError}";
-  };
+  commonConfigPath = pkgs.substituteAll ({
+      name = "common.rasi";
+      src = ./files/common.rasi;
+    }
+    // config.colorScheme.palette);
   rofi = pkgs.rofi-wayland.override {plugins = [pkgs.rofi-calc];};
 in {
   appmenu = pkgs.callPackage ./appmenu.nix {
