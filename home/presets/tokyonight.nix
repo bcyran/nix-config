ca0cfa839567f2931552874508825c5c588efbe0
Bazyli Cyran <bazyli@cyran.dev>
2024-06-11 19:37:38 +0000

    refactor(theme): Rework tokyonight base16 colors

--- home/presets/tokyonight.nix
+++ home/presets/tokyonight.nix
@@ -5,27 +5,27 @@
     name = "Tokyo Night Moon";
     author = "Folke Lemaitre";
     palette = rec {
-      # Terminal & CLI
-      bg = "#222436";
-      fg = "#c8d3f5";
-      base00 = "#1b1d2b";
-      base01 = "#ff757f";
-      base02 = "#c3e88d";
-      base03 = "#ffc777";
-      base04 = "#82aaff";
-      base05 = "#c099ff";
-      base06 = "#86e1fc";
-      base07 = "#828bb8";
-      base08 = "#444a73";
-      base09 = "#ff757f";
-      base0A = "#c3e88d";
-      base0B = "#ffc777";
-      base0C = "#82aaff";
-      base0D = "#c099ff";
-      base0E = "#86e1fc";
-      base0F = "#c8d3f5";
+      base00 = "#222436"; # Default background
+      base01 = "#2f334d"; # Light background
+      base02 = "#2d3f76"; # Selection background
+      base03 = "#5d66a0"; # Comments
+      base04 = "#828bb8"; # Dark foreground
+      base05 = "#c8d3f5"; # Default foreground
+      base06 = "#3760bf"; # Light foreground
+      base07 = "#e1e2e7"; # Light background
+      base08 = "#ff757f"; # Red; variables, XML Tags
+      base09 = "#ff966c"; # Orange; integers, boolean, constants, XML attributes
+      base0A = "#ffc777"; # Yellow; classes
+      base0B = "#c3e88d"; # Green; strings
+      base0C = "#4fd6be"; # Teal; regex, escape characters
+      base0D = "#7aa2f7"; # Blue; functions, methods
+      base0E = "#fca7ea"; # Magenta; keywords
+      base0F = "#c53b53"; # Purple; deprecated, embedded language
 
       # GUI
+      bg = "#222436";
+      fg = "#c8d3f5";
+      comment = "#565f89";
       guiBg = bg;
       guiBgDark = "#1e2030";
       guiBgHighlight = "#2f334d";
