dbcbb08f402d43e1c5be95060e088d42d7430951
Bazyli Cyran <bazyli@cyran.dev>
2024-07-20 08:03:58 +0200

    chore(ddcci): Add ddcutil package

--- system/features/ddcci.nix
+++ system/features/ddcci.nix
@@ -1,4 +1,11 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   boot.kernelModules = ["i2c-dev" "ddcci_backlight"];
   boot.extraModulePackages = [config.boot.kernelPackages.ddcci-driver];
+  environment.systemPackages = with pkgs; [
+    ddcutil
+  ];
 }
