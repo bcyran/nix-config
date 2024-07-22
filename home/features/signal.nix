946280cefc7059f538751f040386dca653813bff
Bazyli Cyran <bazyli@cyran.dev>
2024-07-22 21:05:05 +0200

    feat(signal-desktop): Setup autostart

--- home/features/signal.nix
+++ home/features/signal.nix
@@ -1,20 +1,21 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    signal-desktop
-  ];
-  xdg.configFile."Signal/ephemeral.json" = {
-    text = ''
-      {
-        "localeOverride": null,
-        "system-tray-setting": "DoNotUseSystemTray",
-        "theme-setting": "dark",
-        "spell-check": true,
-        "window": {
-          "maximized": false,
-          "autoHideMenuBar": true,
-          "fullscreen": false
-        }
+{pkgs, ...}: let
+  signalPackage = pkgs.signal-desktop;
+  signalDesktopName = "signal-desktop.desktop";
+  signalDesktop = "${signalPackage}/share/applications/${signalDesktopName}";
+in {
+  home.packages = [signalPackage];
+  xdg.configFile."Signal/ephemeral.json".text = ''
+    {
+      "localeOverride": null,
+      "system-tray-setting": "DoNotUseSystemTray",
+      "theme-setting": "dark",
+      "spell-check": true,
+      "window": {
+        "maximized": false,
+        "autoHideMenuBar": true,
+        "fullscreen": false
       }
-    '';
-  };
+    }
+  '';
+  xdg.configFile."autostart/${signalDesktopName}".source = signalDesktop;
 }
