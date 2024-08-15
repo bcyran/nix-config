ece6d187e4fa3bed2261111f59bbbb61aa2fef3c
Bazyli Cyran <bazyli@cyran.dev>
2024-08-15 10:31:20 +0200

    feat(neovim): Disable inlay hints by default

--- home/features/neovim/files/lua/plugins/override.lua
+++ home/features/neovim/files/lua/plugins/override.lua
@@ -15,4 +15,10 @@ return {
     "williamboman/mason-lspconfig.nvim",
     enabled = vim.fn.executable("nix") ~= 1,
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
 }
