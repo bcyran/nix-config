739e6e0f13fccb287e1b9605df1740a09ebcb578
Bazyli Cyran <bazyli@cyran.dev>
2024-08-16 10:42:56 +0200

    feat(onagre): Add enable option to the module

--- home/features/onagre/default.nix
+++ home/features/onagre/default.nix
@@ -1,16 +1,22 @@
 {
   pkgs,
   config,
+  lib,
   ...
-}: let
-  theme = pkgs.substituteAll ({
+}:
+with lib; let
+  cfg = config.my.programs.onagre;
+
+  themeFile = pkgs.substituteAll ({
       name = "theme.scss";
       src = ./files/theme.scss;
     }
     // config.colorScheme.palette);
 in {
-  home.packages = [pkgs.onagre];
-  xdg.configFile."onagre/theme.scss" = {
-    source = theme;
+  options.my.programs.onagre.enable = mkEnableOption "onagre";
+
+  config = mkIf cfg.enable {
+    home.packages = [pkgs.onagre];
+    xdg.configFile."onagre/theme.scss".source = themeFile;
   };
 }
