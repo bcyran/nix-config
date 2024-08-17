bd426bcea5f4a5fb97a329ffb4db0d7e15943a60
Bazyli Cyran <bazyli@cyran.dev>
2024-08-17 07:21:29 +0200

    feat(btrbk): Add enable option to the module

--- system/features/btrbk.nix
+++ system/features/btrbk.nix
@@ -1,16 +1,27 @@
 {
-  services.btrbk.instances = {
-    home = {
-      onCalendar = "hourly";
-      settings = {
-        volume."/" = {
-          subvolume = "/home";
-          snapshot_dir = "/.snapshots";
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.my.programs.btrbk;
+in {
+  options.my.programs.btrbk.enable = mkEnableOption "btrbk";
+
+  config = mkIf cfg.enable {
+    services.btrbk.instances = {
+      home = {
+        onCalendar = "hourly";
+        settings = {
+          volume."/" = {
+            subvolume = "/home";
+            snapshot_dir = "/.snapshots";
+          };
+          snapshot_preserve = "7d";
+          snapshot_preserve_min = "2d";
+          archive_preserve = "7d";
+          archive_preserve_min = "2d";
         };
-        snapshot_preserve = "7d";
-        snapshot_preserve_min = "2d";
-        archive_preserve = "7d";
-        archive_preserve_min = "2d";
       };
     };
   };
--- system/hosts/slimbook/default.nix
+++ system/hosts/slimbook/default.nix
@@ -48,6 +48,7 @@
     };
     programs = {
       hyprland.enable = true;
+      btrbk.enable = true;
     };
   };
 
