d76c1639c73fad43da96e717ab3e2afb4b54927d
Bazyli Cyran <bazyli@cyran.dev>
2024-08-17 09:30:30 +0200

    refactor(flake): Move system/features to modules/nixos

--- system/hosts/nixtest/default.nix
+++ system/hosts/nixtest/default.nix
@@ -1,16 +1,10 @@
 {
-  pkgs,
   inputs,
+  outputs,
   ...
 }: {
-  # You can import other NixOS modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/nixos):
-    # outputs.nixosModules.example
-
-    # Or modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
+    outputs.nixosModules.default
 
     # Hardware
     ./disks.nix
@@ -20,46 +14,48 @@
     inputs.hardware.nixosModules.common-pc-laptop
 
     # Presets
-    ../../presets/core.nix
     ../../presets/users/bazyli.nix
-
-    # Features
-    ../../features/lanzaboote.nix
-    ../../features/locale.nix
-    ../../features/console.nix
-    ../../features/utilities.nix
-    ../../features/greetd.nix
-    ../../features/networking.nix
-    ../../features/hyprland
-    ../../features/lock.nix
-    ../../features/pipewire.nix
-    ../../features/filesystem.nix
-    ../../features/bluetooth.nix
-    ../../features/virtualisation.nix
-    ../../features/logiops.nix
-    ../../features/printing.nix
-    ../../features/silentboot.nix
-    ../../features/ddcci.nix
-    ../../features/tlp.nix
-    ../../features/polkit.nix
-    ../../features/upower.nix
   ];
+
+  my = {
+    configurations = {
+      core.enable = true;
+      lanzaboote.enable = true;
+      networking.enable = true;
+      console.enable = true;
+      locale.enable = true;
+      bluetooth.enable = true;
+      audio.enable = true;
+      ddcci.enable = true;
+      filesystem.enable = true;
+      printing.enable = true;
+      silentboot.enable = true;
+      virtualisation.enable = true;
+    };
+    programs = {
+      hyprland.enable = true;
+      btrbk.enable = true;
+      greetd.enable = true;
+      logiops.enable = true;
+      tlp.enable = true;
+      upower.enable = true;
+    };
+  };
 
   networking.hostName = "nixtest";
 
   programs.fish.enable = true;
   programs.light.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    neovim
-  ];
+  services.hardware.bolt.enable = true;
+  services.systemd-lock-handler.enable = true; # Required for `lock.target` in user's systemd
 
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
+  security.pam.services.hyprlock.text = "auth include login"; # Required by `hyprlock`
+  security.polkit.enable = true;
+
   services.openssh = {
     enable = true;
     settings = {
-      # Forbid root login through SSH.
       PermitRootLogin = "no";
     };
   };
--- system/hosts/slimbook/default.nix
+++ system/hosts/slimbook/default.nix
@@ -1,12 +1,10 @@
 {
-  pkgs,
   inputs,
+  outputs,
   ...
 }: {
-  # You can import other NixOS modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/nixos):
-    # outputs.nixosModules.example
+    outputs.nixosModules.default
 
     # Hardware
     ./disks.nix
@@ -17,26 +15,6 @@
 
     # Presets
     ../../presets/users/bazyli.nix
-
-    # Features
-    ../../features/core.nix
-    ../../features/lanzaboote.nix
-    ../../features/locale.nix
-    ../../features/console.nix
-    ../../features/greetd.nix
-    ../../features/networking.nix
-    ../../features/hyprland
-    ../../features/audio.nix
-    ../../features/filesystem.nix
-    ../../features/bluetooth.nix
-    ../../features/virtualisation.nix
-    ../../features/logiops.nix
-    ../../features/printing.nix
-    ../../features/silentboot.nix
-    ../../features/ddcci.nix
-    ../../features/btrbk.nix
-    ../../features/tlp.nix
-    ../../features/upower.nix
   ];
 
   my = {
