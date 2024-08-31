50cc6c0c27f2bf374ef1368ac8aa034abd881542
Bazyli Cyran <bazyli@cyran.dev>
2024-08-31 18:46:01 +0200

    feat(system): Tweak virtualisation config

--- modules/nixos/configurations/virtualisation.nix
+++ modules/nixos/configurations/virtualisation.nix
@@ -12,5 +12,13 @@ in {
     environment.systemPackages = [pkgs.qemu];
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
+
+    users.users.${config.my.user.name}.extraGroups = ["libvirtd"];
+
+    boot.extraModprobeConfig = ''
+      options kvm_intel nested=1
+      options kvm_intel emulate_invalid_guest_state=0
+      options kvm ignore_msrs=1
+    '';
   };
 }
