02957e0a4a7fee3775616e1f8be56a05635031ad
Bazyli Cyran <bazyli@cyran.dev>
2024-08-31 09:17:03 +0200

    feat(scripts): Detect the flake dir name

--- scripts/disko-secureboot-install.sh
+++ scripts/disko-secureboot-install.sh
@@ -20,9 +20,10 @@ trap err_report ERR
 script_path=$(realpath "$0")
 script_name=$(basename "${script_path}")
 flake_dir=$(realpath "$(dirname "${script_path}")/..")
+flake_dir_name=$(basename "${flake_dir}")
 target_host="${1:-}"
 target_user="${2:-bazyli}"
-target_config_dir="${MOUNT_DIR}/home/${target_user}/nixos-config"
+target_config_dir="${MOUNT_DIR}/home/${target_user}/${flake_dir_name}"
 
 if [[ "$(id -u)" -eq 0 ]]; then
   echo -e "${RED}ERROR!${ENDCOLOR} ${script_name} should be run as a regular user!" >&2
@@ -53,7 +54,7 @@ echo -e "${BLUE}Installing NixOS${ENDCOLOR}"
 sudo mkdir ${INSTALLER_TMP_DIR}
 sudo TMPDIR=${INSTALLER_TMP_DIR} nixos-install --flake ".#${target_host}"
 
-echo -e "${BLUE}Copying the config to the ${target_user}'s home directory${ENDCOLOR}"
+echo -e "${BLUE}Copying the config to '${target_config_dir}'${ENDCOLOR}"
 mkdir -p "${target_config_dir}"
 rsync -a "${flake_dir}/" "${target_config_dir}"
 
