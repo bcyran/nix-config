0e106302720cd9a47d9400109b4157dec2d551ee
Bazyli Cyran <bazyli@cyran.dev>
2024-09-19 21:26:43 +0200

    refactor(btrbk): Keep more snapshots and backups

--- modules/nixos/programs/btrbk.nix
+++ modules/nixos/programs/btrbk.nix
@@ -16,10 +16,10 @@ in {
             subvolume = "/home";
             snapshot_dir = "/.snapshots";
           };
-          snapshot_preserve = "7d";
-          snapshot_preserve_min = "2d";
-          archive_preserve = "7d";
-          archive_preserve_min = "2d";
+          snapshot_preserve = "14d";
+          snapshot_preserve_min = "3d";
+          archive_preserve = "14d";
+          archive_preserve_min = "3d";
         };
       };
     };
