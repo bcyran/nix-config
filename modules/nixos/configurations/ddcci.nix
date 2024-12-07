f0d8b240de368774658cc20008b392c63678370f
Bazyli Cyran <bazyli@cyran.dev>
2024-12-07 11:33:43 +0100

    refactor(ddcci): Remove forced device detection
    It seems to work without forced detection again and the systemd units
    fail on every boot.

--- modules/nixos/configurations/ddcci.nix
+++ modules/nixos/configurations/ddcci.nix
@@ -5,9 +5,6 @@
   ...
 }: let
   cfg = config.my.configurations.ddcci;
-
-  ddcutilBin = "${pkgs.ddcutil}/bin/ddcutil";
-  sleepBin = "${pkgs.coreutils}/bin/sleep";
 in {
   options.my.configurations.ddcci.enable = lib.mkEnableOption "ddcci";
 
@@ -18,28 +15,5 @@ in {
     };
 
     environment.systemPackages = [pkgs.ddcutil];
-
-    # Source: https://discourse.nixos.org/t/brightness-control-of-external-monitors-with-ddcci-backlight/8639/15
-    services.udev.extraRules = ''
-      SUBSYSTEM=="i2c-dev", ACTION=="add",\
-        TAG+="ddcci",\
-        TAG+="systemd",\
-        ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
-    '';
-
-    systemd.services."ddcci@" = {
-      description = "Force DDC/CI device detection";
-      scriptArgs = "%i";
-      script = ''
-        id=$(echo "$1" | cut -d "-" -f 2)
-        if ${ddcutilBin} getvcp 10 -b "$id"; then
-          echo 'ddcci 0x37' > "/sys/bus/i2c/devices/$1/new_device"
-        fi
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStartPre = "${sleepBin} 10";
-      };
-    };
   };
 }
