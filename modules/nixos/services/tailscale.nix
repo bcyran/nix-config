d24d3abc401cf3947407dfe6cd75652bc82ffa79
Bazyli Cyran <bazyli@cyran.dev>
2024-12-21 23:49:32 +0100

    feat(tailscale): Add support for authKey and routes advertisement

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -6,10 +6,41 @@
 }: let
   cfg = config.my.services.tailscale;
 in {
-  options.my.services.tailscale.enable = lib.mkEnableOption "tailscale";
+  options.my.services.tailscale = {
+    enable = lib.mkEnableOption "tailscale";
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [pkgs.tailscale];
-    services.tailscale.enable = true;
+    authKeyFile = lib.mkOption {
+      type = with lib.types; nullOr path;
+      default = null;
+      description = "Path to the Tailscale authentication key file.";
+    };
+
+    advertiseRoutes = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "Routes to expose to the Tailscale network.";
+      example = ["192.168.0.100/32"];
+    };
   };
+
+  config =
+    lib.mkIf cfg.enable
+    {
+      environment.systemPackages = [pkgs.tailscale];
+
+      services.tailscale = let
+        advertiseRoutesEnabled = cfg.advertiseRoutes != [];
+      in {
+        enable = true;
+        inherit (cfg) authKeyFile;
+        useRoutingFeatures =
+          if advertiseRoutesEnabled
+          then "server"
+          else "none";
+        extraUpFlags = lib.mkIf advertiseRoutesEnabled [
+          "--accept-dns=false"
+          "--advertise-routes ${lib.concatStringsSep "," cfg.advertiseRoutes}"
+        ];
+      };
+    };
 }
