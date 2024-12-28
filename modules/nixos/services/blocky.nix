15d9dd8869d4591adf759f14ccdfb530dac77717
Bazyli Cyran <bazyli@cyran.dev>
2024-12-28 09:18:29 +0100

    feat(blocky): Provision Prometheus job and Grafana dashboard

--- modules/nixos/services/blocky.nix
+++ modules/nixos/services/blocky.nix
@@ -1,9 +1,15 @@
 {
+  inputs,
+  pkgs,
   config,
   lib,
   ...
 }: let
+  grafanaDashboardsLib = inputs.grafana-dashboards.lib {inherit pkgs;};
   cfg = config.my.services.blocky;
+
+  blockyUrl = "127.0.0.1:${toString cfg.httpPort}";
+  blockyPrometheusJobName = "blocky";
 in {
   options.my.services.blocky = {
     enable = lib.mkEnableOption "blocky";
@@ -32,37 +38,80 @@ in {
       allowedUDPPorts = [cfg.dnsPort];
     };
 
-    services.blocky = {
-      enable = true;
-      settings = {
-        ports = {
-          dns = cfg.dnsPort;
-          http = cfg.httpPort;
+    services = {
+      blocky = {
+        enable = true;
+        settings = {
+          ports = {
+            dns = cfg.dnsPort;
+            http = cfg.httpPort;
+          };
+          upstreams.groups.default = [
+            "https://cloudflare-dns.com/dns-query"
+          ];
+          bootstrapDns = {
+            upstream = "https://cloudflare-dns.com/dns-query";
+            ips = ["1.1.1.1" "1.0.0.1"];
+          };
+          blocking = {
+            denylists = {
+              ads = [
+                "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+              ];
+            };
+            clientGroupsBlock = {
+              default = ["ads"];
+            };
+          };
+          caching = {
+            minTime = "5m";
+            maxTime = "30m";
+            prefetching = true;
+          };
+          customDNS.mapping = cfg.customDNSMappings;
+          prometheus.enable = true;
         };
-        upstreams.groups.default = [
-          "https://cloudflare-dns.com/dns-query"
-        ];
-        bootstrapDns = {
-          upstream = "https://cloudflare-dns.com/dns-query";
-          ips = ["1.1.1.1" "1.0.0.1"];
-        };
-        blocking = {
-          denylists = {
-            ads = [
-              "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+      };
+
+      prometheus.scrapeConfigs = lib.mkIf config.services.prometheus.enable [
+        {
+          job_name = blockyPrometheusJobName;
+          static_configs = [
+            {
+              targets = [blockyUrl];
+            }
+          ];
+        }
+      ];
+
+      grafana = lib.mkIf config.services.grafana.enable {
+        settings.panels.disable_sanitize_html = true;
+
+        provision.dashboards.settings.providers = [
+          (grafanaDashboardsLib.dashboardEntry {
+            name = "blocky";
+            path = grafanaDashboardsLib.fetchDashboard {
+              name = "blocky";
+              id = 13768;
+              version = 4;
+              hash = "sha256-61Rapsit6gpG8GtnNpO3jckZwfDkzZtFFaFMVStAf6U=";
+            };
+            transformations = grafanaDashboardsLib.fillTemplating [
+              {
+                key = "DS_PROMETHEUS";
+                value = "Prometheus";
+              }
+              {
+                key = "job";
+                value = blockyPrometheusJobName;
+              }
+              {
+                key = "blocky_url";
+                value = "";
+              }
             ];
-          };
-          clientGroupsBlock = {
-            default = ["ads"];
-          };
-        };
-        caching = {
-          minTime = "5m";
-          maxTime = "30m";
-          prefetching = true;
-        };
-        customDNS.mapping = cfg.customDNSMappings;
-        prometheus.enable = true;
+          })
+        ];
       };
     };
   };
