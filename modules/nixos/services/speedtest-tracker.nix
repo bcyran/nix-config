c4db88f7d5b496a1a0ecd0977948432007b7a66c
Bazyli Cyran <bazyli@cyran.dev>
2024-12-31 01:01:20 +0100

    refactor(speedtest-tracker): Run as a dedicated user

--- modules/nixos/services/speedtest-tracker.nix
+++ modules/nixos/services/speedtest-tracker.nix
@@ -6,6 +6,7 @@
   cfg = config.my.services.speedtest-tracker;
 
   speedtestTrackerVersion = "v1.0.2";
+  dataDir = "/var/lib/speedtest-tracker";
 in {
   options = {
     my.services.speedtest-tracker = {
@@ -34,26 +35,35 @@ in {
   config = lib.mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = [cfg.port];
 
-    virtualisation.oci-containers.containers = {
-      speedtest-tracker = {
-        image = "lscr.io/linuxserver/speedtest-tracker:version-${speedtestTrackerVersion}";
-        autoStart = true;
-        ports = ["127.0.0.1:${builtins.toString cfg.port}:80"];
-        volumes = [
-          "/var/lib/speedtest-tracker:/config"
-        ];
-        environment = {
-          PUID = "1000";
-          PGID = "1000";
-          DB_CONNECTION = "sqlite";
-          DISPLAY_TIMEZONE = "Europe/Warsaw";
-          DATETIME_FORMAT = "d.m.y H:i:s";
-          CHART_DATETIME_FORMAT = "d.m H:i";
-          SPEEDTEST_SCHEDULE = "0 * * * *";
-          PUBLIC_DASHBOARD = "true";
-        };
-        environmentFiles = [cfg.environmentFile];
+    virtualisation.oci-containers.containers.speedtest-tracker = {
+      image = "lscr.io/linuxserver/speedtest-tracker:version-${speedtestTrackerVersion}";
+      autoStart = true;
+      ports = ["127.0.0.1:${builtins.toString cfg.port}:80"];
+      volumes = [
+        "${dataDir}:/config"
+      ];
+      environment = {
+        PUID = toString config.users.users.speedtest-tracker.uid;
+        PGID = toString config.users.groups.speedtest-tracker.gid;
+        DB_CONNECTION = "sqlite";
+        DISPLAY_TIMEZONE = "Europe/Warsaw";
+        DATETIME_FORMAT = "d.m.y H:i:s";
+        CHART_DATETIME_FORMAT = "d.m H:i";
+        SPEEDTEST_SCHEDULE = "0 * * * *";
+        PUBLIC_DASHBOARD = "true";
       };
+      environmentFiles = [cfg.environmentFile];
+    };
+
+    users = rec {
+      users.speedtest-tracker = {
+        home = dataDir;
+        createHome = true;
+        group = "speedtest-tracker";
+        uid = 2001;
+        isSystemUser = true;
+      };
+      groups.speedtest-tracker.gid = users.speedtest-tracker.uid;
     };
 
     my.services.reverseProxy.virtualHosts.${cfg.domain} = {
