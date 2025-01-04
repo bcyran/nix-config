c73871c6d507d9bf37dfc8e9fe1d08971cc3ddbf
Bazyli Cyran <bazyli@cyran.dev>
2025-01-04 13:35:51 +0100

    refactor(hosts/t480): Drop my.programs.btrbk module usage

--- hosts/t480/nixos/configuration.nix
+++ hosts/t480/nixos/configuration.nix
@@ -61,16 +61,33 @@
       sops.enable = true;
     };
     programs = {
-      btrbk = {
-        enableSystemSnapshots = true;
-        systemBackupTarget = "/mnt/backup/t480";
-      };
       podman.enable = true;
     };
   };
 
-  services = {
-    hardware.bolt.enable = true;
+  services.hardware.bolt.enable = true;
+
+  services.btrbk.instances.system = let
+    snapshotRetention = "14d";
+    snapshotRetentionMin = "1d";
+  in {
+    onCalendar = "hourly";
+    settings = {
+      volume."/" = {
+        subvolume = {
+          "/" = {};
+          "/var" = {};
+        };
+        snapshot_dir = "/.snapshots";
+        target = "/mnt/backup/t480";
+        target_preserve = snapshotRetention;
+        target_preserve_min = snapshotRetentionMin;
+      };
+      snapshot_preserve = snapshotRetention;
+      snapshot_preserve_min = snapshotRetentionMin;
+      archive_preserve = snapshotRetention;
+      archive_preserve_min = snapshotRetentionMin;
+    };
   };
 
   networking.networkmanager.ensureProfiles = {
