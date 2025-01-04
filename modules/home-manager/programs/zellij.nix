f5d204d94549a2246808a30c46b58978261eff42
Bazyli Cyran <bazyli@cyran.dev>
2025-01-04 11:43:21 +0100

    feat(zellij): Adjust bindings
    Default zellij bindings conflict heavily with my nvim and fish bindings.
    However, tmux is annoying me recently with many small things which
    zellij handles flawlessly so I'm trying to replicate tmux in zellij...

--- modules/home-manager/programs/zellij.nix
+++ modules/home-manager/programs/zellij.nix
@@ -9,26 +9,116 @@ in {
   options.my.programs.zellij.enable = lib.mkEnableOption "zellij";
 
   config = lib.mkIf cfg.enable {
-    programs.zellij = {
-      enable = true;
-      settings = {
-        theme = "custom";
-        themes.custom = {
-          fg = "#${palette.base05}";
-          bg = "#${palette.base00}";
-          black = "#${palette.base00}";
-          red = "#${palette.base08}";
-          green = "#${palette.base0B}";
-          yellow = "#${palette.base0A}";
-          blue = "#${palette.base0D}";
-          magenta = "#${palette.base0E}";
-          cyan = "#${palette.base0C}";
-          white = "#${palette.base05}";
-          orange = "#${palette.base09}";
-        };
-        copy_command = "wl-copy";
-        pane_frames = false;
-      };
-    };
+    programs.zellij.enable = true;
+    xdg.configFile."zellij/config.kdl".text = ''
+      pane_frames false
+
+      keybinds clear-defaults=true {
+        normal {
+          bind "Alt h" { MoveFocusOrTab "Left"; }
+          bind "Alt l" { MoveFocusOrTab "Right"; }
+          bind "Alt j" { MoveFocus "Down"; }
+          bind "Alt k" { MoveFocus "Up"; }
+
+          bind "Alt H" { Resize "Increase Left"; }
+          bind "Alt L" { Resize "Increase Right"; }
+          bind "Alt J" { Resize "Increase Down"; }
+          bind "Alt K" { Resize "Increase Up"; }
+
+
+          bind "Alt Tab" { ToggleTab; }
+        }
+        tmux  {
+          bind "Ctrl a" "Esc" { Write 2; SwitchToMode "Normal"; }
+
+          bind "c" { NewTab; SwitchToMode "Normal"; }
+          bind "n" { GoToNextTab; SwitchToMode "Normal"; }
+          bind "p" { GoToPreviousTab; SwitchToMode "Normal"; }
+          bind "," { SwitchToMode "RenameTab"; TabNameInput 0; }
+          bind "Tab" { ToggleTab; SwitchToMode "Normal"; }
+
+          bind "|" { NewPane  "Right"; SwitchToMode "Normal"; }
+          bind "-" { NewPane  "Down"; SwitchToMode "Normal"; }
+          bind "." { SwitchToMode "RenamePane"; PaneNameInput 0; }
+          bind "z" { ToggleFocusFullscreen; SwitchToMode "Normal"; }
+
+          bind "h" { MoveFocusOrTab "Left"; SwitchToMode "Normal"; }
+          bind "l" { MoveFocusOrTab "Right"; SwitchToMode "Normal"; }
+          bind "j" { MoveFocus "Down"; SwitchToMode "Normal"; }
+          bind "k" { MoveFocus "Up"; SwitchToMode "Normal"; }
+
+          bind "v" { SwitchToMode "Scroll"; }
+          bind "Space" { NextSwapLayout; }
+          bind "x" { CloseFocus; SwitchToMode "Normal"; }
+
+          bind "d" { Detach; }
+          bind "Ctrl q" { Quit; }
+        }
+        renametab {
+          bind "Ctrl a" "Enter" { SwitchToMode "Normal"; }
+          bind "Esc" { UndoRenameTab; SwitchToMode "Normal"; }
+        }
+        renamepane {
+          bind "Ctrl a" "Enter" { SwitchToMode "Normal"; }
+          bind "Esc" { UndoRenamePane; SwitchToMode "Pane"; }
+        }
+        scroll {
+          bind "e" { EditScrollback; SwitchToMode "Normal"; }
+          bind "s" { SwitchToMode "EnterSearch"; SearchInput 0; }
+          bind "Ctrl c" { ScrollToBottom; SwitchToMode "Normal"; }
+
+          bind "j" { ScrollDown; }
+          bind "k" { ScrollUp; }
+          bind "Ctrl f" { PageScrollDown; }
+          bind "Ctrl b" { PageScrollUp; }
+          bind "Ctrl d" { HalfPageScrollDown; }
+          bind "Ctrl u" { HalfPageScrollUp; }
+        }
+        entersearch {
+          bind "Esc" "Ctrl c" { ScrollToBottom; SwitchToMode "Scroll"; }
+          bind "Enter" { SwitchToMode "Search"; }
+        }
+        search {
+          bind "Esc" "Ctrl c" { ScrollToBottom; SwitchToMode "Scroll"; }
+
+          bind "j" { ScrollDown; }
+          bind "k" { ScrollUp; }
+          bind "Ctrl f" { PageScrollDown; }
+          bind "Ctrl b" { PageScrollUp; }
+          bind "Ctrl d" { HalfPageScrollDown; }
+          bind "Ctrl u" { HalfPageScrollUp; }
+
+          bind "n" { Search "down"; }
+          bind "p" { Search "up"; }
+
+          bind "c" { SearchToggleOption "CaseSensitivity"; }
+          bind "w" { SearchToggleOption "Wrap"; }
+          bind "o" { SearchToggleOption "WholeWord"; }
+        }
+        shared_except "tmux" "locked" {
+          bind "Ctrl a" "Alt a" { SwitchToMode "Tmux"; }
+        }
+        shared_except "normal" {
+          bind "Esc" { Write 2; SwitchToMode "Normal"; }
+        }
+      }
+
+      themes {
+        custom {
+          bg "#${palette.base00}"
+          black "#${palette.base00}"
+          blue "#${palette.base0D}"
+          cyan "#${palette.base0C}"
+          fg "#${palette.base05}"
+          green "#${palette.base0B}"
+          magenta "#${palette.base0E}"
+          orange "#${palette.base09}"
+          red "#${palette.base08}"
+          white "#${palette.base05}"
+          yellow "#${palette.base0A}"
+        }
+      }
+      theme "custom"
+    '';
   };
 }
