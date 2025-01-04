0a604bc5c392cbf3356b8ea314fc64733e124d8c
Bazyli Cyran <bazyli@cyran.dev>
2025-01-04 13:36:33 +0100

    feat(hosts/t480): Setup receiving backups from slimbook

--- hosts/t480/nixos/services.nix
+++ hosts/t480/nixos/services.nix
@@ -128,4 +128,11 @@ in {
       openFirewall = true;
     };
   };
+
+  services.btrbk.sshAccess = [
+    {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8a552vyvnPoS/JEkSujoygzQw0cB8jO2yI8VlsLUF6 btrbk@slimbook";
+      roles = ["info" "receive"];
+    }
+  ];
 }
