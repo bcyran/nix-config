a5420a39c6cd098c1685d326b78be8788b89c027
Bazyli Cyran <bazyli@cyran.dev>
2025-01-28 20:00:18 +0100

    feat(nixos): Setup PostgreSQL Prometheus exporter and Grafana dashboard

--- modules/nixos/services/postgresql.nix
+++ modules/nixos/services/postgresql.nix
@@ -1,13 +1,21 @@
 {
+  inputs,
+  pkgs,
   my,
   config,
   lib,
   ...
 }: let
   cfg = config.my.services.postgresql;
+  prometheusCfg = config.my.services.prometheus;
+
+  grafanaDashboardsLib = inputs.grafana-dashboards.lib {inherit pkgs;};
 
   effectiveDataDir = "${cfg.dataDir}/${config.services.postgresql.package.psqlSchema}";
   dumpDir = "${cfg.dataDir}/dump";
+
+  postgresExporterUser = "postgres-exporter";
+  postgresExporterPort = 9187;
 in {
   options.my.services.postgresql = let
     serviceName = "PostgreSQL";
@@ -28,6 +36,15 @@ in {
           listen_addresses = lib.mkForce cfg.address;
           inherit (cfg) port;
         };
+
+        ensureUsers = lib.mkIf prometheusCfg.enable [
+          {
+            name = postgresExporterUser;
+          }
+        ];
+        authentication = lib.mkIf prometheusCfg.enable ''
+          local postgres ${postgresExporterUser} trust
+        '';
       };
 
       postgresqlBackup = {
@@ -36,6 +53,47 @@ in {
         compression = "zstd";
         location = dumpDir;
         startAt = "*-*-* 00:00:00";
+      };
+
+      prometheus = lib.mkIf prometheusCfg.enable {
+        exporters.postgres = {
+          enable = true;
+          user = postgresExporterUser;
+          dataSourceName = "postgresql:///postgres?host=/run/postgresql";
+          port = postgresExporterPort;
+        };
+        scrapeConfigs = [
+          {
+            job_name = "postgres";
+            static_configs = [
+              {
+                targets = ["127.0.0.1:${toString postgresExporterPort}"];
+              }
+            ];
+          }
+        ];
+      };
+
+      grafana = lib.mkIf config.services.grafana.enable {
+        settings.panels.disable_sanitize_html = true;
+
+        provision.dashboards.settings.providers = [
+          (grafanaDashboardsLib.dashboardEntry {
+            name = "postgres";
+            path = grafanaDashboardsLib.fetchDashboard {
+              name = "postgres";
+              id = 9628;
+              version = 8;
+              hash = "sha256-VDX4BfGbY3PW5YHcRilg4g6+6uN22EK5+r+Ph67xXAY";
+            };
+            transformations = grafanaDashboardsLib.fillTemplating [
+              {
+                key = "DS_PROMETHEUS";
+                value = "Prometheus";
+              }
+            ];
+          })
+        ];
       };
     };
 
