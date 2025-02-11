fde939d1cc83900cb5e0184a5e5afaac5a22de17
Bazyli Cyran <bazyli@cyran.dev>
2025-02-11 19:30:59 +0100

    feat(hosts/slimbook): Create wireguard profile for connecting from inside the LAN

--- hosts/slimbook/nixos/wireguard.nix
+++ hosts/slimbook/nixos/wireguard.nix
@@ -17,19 +17,35 @@ in {
     environmentFiles = with config.sops.secrets; [
       wireguard_env_file.path
     ];
-    profiles = {
-      vps = mkWireguardProfile {
-        id = "vps";
+    profiles = let
+      vpsProfileCommon = {
         interfaceName = "wg0";
         address = mkCidr peers.slimbook.ip 24;
         privateKey = "$WIREGUARD_PRIVATE_KEY";
         peerEndpoint = wireguard.endpoint;
         peerPublicKey = peers.vps.publicKey;
-        peerAllowedIPs = [
-          wireguard.subnet
-          (mkCidr devices.homelab.ip 32)
-        ];
       };
+    in {
+      # Connection for when I'm inside my home LAN.
+      # Only wireguard subnet is routed through the tunnel.
+      vps-inside = mkWireguardProfile ({
+          id = "vps-inside";
+          peerAllowedIPs = [
+            wireguard.subnet
+          ];
+        }
+        // vpsProfileCommon);
+      # Connection for when I'm outside my home LAN.
+      # Additionally to the wireguard subnet, homelab connection is routed through the tunnel as well.
+      # This way I can access my homelab using its LAN IP, like I'm at home.
+      vps-outside = mkWireguardProfile ({
+          id = "vps-outside";
+          peerAllowedIPs = [
+            wireguard.subnet
+            (mkCidr devices.homelab.ip 32)
+          ];
+        }
+        // vpsProfileCommon);
     };
   };
 }
