7d0a1a4b8722bfdfcdc172463b72b307d4e290d4
Bazyli Cyran <bazyli@cyran.dev>
2025-02-23 14:19:31 +0100

    feat(fish): Allow using aliases with sudo

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -102,6 +102,22 @@ in {
                     return 1
             end
           '';
+          sudo = ''
+            for i in (seq 1 (count $argv))
+                if command -q -- $argv[$i]
+                    command sudo $argv
+                    return
+                else if functions -q -- $argv[$i]
+                    if test $i != 1
+                        set sudo_args $argv[..(math $i - 1)]
+                    end
+                    command sudo $sudo_args -E fish -C "source $(functions --no-details (functions | string split ', ') | psub)" -c '$argv' $argv[$i..]
+                    return
+                end
+            end
+
+            command sudo $argv
+          '';
         };
         interactiveShellInit = ''
           bind / _atuin_search
