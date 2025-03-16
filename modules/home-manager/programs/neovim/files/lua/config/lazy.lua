170b46f9daa98d27f102f647306f6b02b3026720
Bazyli Cyran <bazyli@cyran.dev>
2025-03-16 16:12:34 +0100

    feat(nvim): Enable kotlin support

--- modules/home-manager/programs/neovim/files/lua/config/lazy.lua
+++ modules/home-manager/programs/neovim/files/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.kotlin" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "plugins.lang" },
