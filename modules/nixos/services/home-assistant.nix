40348ef3219a069ab6ea272cc8d5c305e92ce425
Bazyli Cyran <bazyli@cyran.dev>
2025-05-06 20:03:24 +0200

    feat(home-assistant): Setup recorder and history

--- modules/nixos/services/home-assistant.nix
+++ modules/nixos/services/home-assistant.nix
@@ -6,6 +6,8 @@
   ...
 }: let
   cfg = config.my.services.home-assistant;
+
+  pgDatabase = "hass";
 in {
   options.my.services.home-assistant = let
     serviceName = "Home Assistant";
@@ -18,64 +20,85 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.home-assistant = {
-      enable = true;
-      inherit (cfg) openFirewall;
-
-      config = {
-        homeassistant = {
-          latitude = "!secret home_latitute";
-          longitude = "!secret home_longitude";
-          country = "PL";
-          unit_system = "metric";
-          internal_url = "https://${cfg.reverseProxy.domain}";
-        };
-        lovelace.mode = "yaml";
-        http = {
-          use_x_forwarded_for = true;
-          server_host = cfg.address;
-          server_port = cfg.port;
-          trusted_proxies = ["127.0.0.1"];
-        };
-        scene = "!include scenes.yaml";
-        automation = "!include automations.yaml";
-        script = "!include scripts.yaml";
-
-        mobile_app = {};
-
-        fan = [
+    services = {
+      postgresql = {
+        ensureDatabases = [pgDatabase];
+        ensureUsers = [
           {
-            platform = "xiaomi_miio_fan";
-            name = "Xiaomi Smart Fan";
-            host = "10.100.100.204";
-            token = "!secret xiaomi_fan_token";
-            model = "zhimi.fan.za5";
+            name = pgDatabase;
+            ensureDBOwnership = true;
           }
         ];
       };
 
-      extraComponents = [
-        "default_config"
-        "met"
-        "google_translate"
-        "mobile_app"
-        "group"
-        "ping"
-        "device_tracker"
-        "media_player"
-        "yeelight"
-        "philips_js"
-        "xiaomi_miio"
-      ];
+      home-assistant = {
+        enable = true;
+        inherit (cfg) openFirewall;
 
-      customComponents = [
-        my.pkgs.xiaomi_miio_fan
-      ];
+        config = {
+          homeassistant = {
+            latitude = "!secret home_latitute";
+            longitude = "!secret home_longitude";
+            country = "PL";
+            unit_system = "metric";
+            internal_url = "https://${cfg.reverseProxy.domain}";
+          };
+          lovelace.mode = "yaml";
+          http = {
+            use_x_forwarded_for = true;
+            server_host = cfg.address;
+            server_port = cfg.port;
+            trusted_proxies = ["127.0.0.1"];
+          };
+          scene = "!include scenes.yaml";
+          automation = "!include automations.yaml";
+          script = "!include scripts.yaml";
 
-      customLovelaceModules = with pkgs.home-assistant-custom-lovelace-modules; [
-        mushroom
-        universal-remote-card
-      ];
+          recorder.db_url = "postgresql://@/hass";
+
+          mobile_app = {};
+          history = {};
+
+          fan = [
+            {
+              platform = "xiaomi_miio_fan";
+              name = "Xiaomi Smart Fan";
+              host = "10.100.100.204";
+              token = "!secret xiaomi_fan_token";
+              model = "zhimi.fan.za5";
+            }
+          ];
+        };
+
+        extraPackages = python3Packages:
+          with python3Packages; [
+            # recorder postgresql support
+            psycopg2
+          ];
+
+        extraComponents = [
+          "default_config"
+          "met"
+          "google_translate"
+          "mobile_app"
+          "group"
+          "ping"
+          "device_tracker"
+          "media_player"
+          "yeelight"
+          "philips_js"
+          "xiaomi_miio"
+        ];
+
+        customComponents = [
+          my.pkgs.xiaomi_miio_fan
+        ];
+
+        customLovelaceModules = with pkgs.home-assistant-custom-lovelace-modules; [
+          mushroom
+          universal-remote-card
+        ];
+      };
     };
 
     my.services.caddy.reverseProxyHosts = my.lib.caddy.mkReverseProxy cfg;
