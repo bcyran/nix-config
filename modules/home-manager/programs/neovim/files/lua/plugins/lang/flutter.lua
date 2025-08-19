253ecb28ddea63bd8dbdcdfdfd849954571fe14e
Bazyli Cyran <bazyli@cyran.dev>
2025-08-19 21:19:48 +0200

    fix(neovim): Unpin dart treesitter parser version
    This was pinned because dart parser had major issue for a looong time.
    Currently, the newest version resolves almost all of them and the pinned
    version stopped working for some other reasons.

--- modules/home-manager/programs/neovim/files/lua/plugins/lang/flutter.lua
+++ modules/home-manager/programs/neovim/files/lua/plugins/lang/flutter.lua
@@ -3,20 +3,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- See:
-      -- * https://github.com/nvim-treesitter/nvim-treesitter/issues/4945
-      -- * https://github.com/nvim-treesitter/nvim-treesitter/issues/5868
-      -- * https://github.com/UserNobody14/tree-sitter-dart/issues/60
-      local parsers = require("nvim-treesitter.parsers").get_parser_configs()
-      ---@diagnostic disable: inject-field
-      parsers.dart = {
-        install_info = {
-          url = "https://github.com/UserNobody14/tree-sitter-dart",
-          files = { "src/parser.c", "src/scanner.c" },
-          revision = "8aa8ab977647da2d4dcfb8c4726341bee26fbce4",
-        },
-      }
-
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
           "dart",
