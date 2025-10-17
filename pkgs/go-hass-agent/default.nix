14f53ebe1ae1a89fbf88f0de64a7aa1146864c93
Bazyli Cyran <bazyli@cyran.dev>
2025-10-17 17:42:14 +0200

    chore(pkgs): Update go-hass-agent

--- pkgs/go-hass-agent/default.nix
+++ pkgs/go-hass-agent/default.nix
@@ -2,89 +2,61 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
-  mage,
-  writeShellScriptBin,
-  pkg-config,
-  libGL,
-  libX11,
-  libXcursor,
-  libXrandr,
-  libXinerama,
-  libXi,
-  libXxf86vm,
+  buildNpmPackage,
 }: let
-  version = "v13.2.6";
-  # Result of `git describe --tags --always --dirty`
-  commitHashShort = "b912d79d";
-  # Result of `git log --date=iso8601-strict -1 --pretty=%ct`
-  commitTimestamp = "1748040984";
+  version = "v14.1.0";
 
-  fakeGit = writeShellScriptBin "git" ''
-    if [[ $@ = "describe --tags --always --dirty" ]]; then
-        echo "${version}"
-    elif [[ $@ = "rev-parse --short HEAD" ]]; then
-        echo "${commitHashShort}"
-    elif [[ $@ = "log --date=iso8601-strict -1 --pretty=%ct" ]]; then
-        echo "${commitTimestamp}"
-    else
-        >&2 echo "Unknown command: $@"
-        exit 1
-    fi
-  '';
-in
-  buildGoModule rec {
-    pname = "go-hass-agent";
-    inherit version;
+  src = fetchFromGitHub {
+    owner = "joshuar";
+    repo = "go-hass-agent";
+    rev = "refs/tags/${version}";
+    hash = "sha256-g+7+a6Wtgqdj8n1D/vPtQLRRVDP155SVSotAsONde/4=";
+  };
 
-    src = fetchFromGitHub {
-      owner = "joshuar";
-      repo = "go-hass-agent";
-      rev = "refs/tags/${version}";
-      hash = "sha256-vO5GUkGePKSaj4UDZioGBHLXx82l/LgmBz+GbfIiBxs=";
-    };
+  frontend = buildNpmPackage {
+    pname = "go-hass-agent-frontend";
+    inherit version src;
 
-    vendorHash = "sha256-HjjRqEeffpavhu/9m6fg6/23VoBjGi7Cfm1O26yj65g=";
+    npmDepsHash = "sha256-m+5xVkYmf/0K5Bi4IQEBd82r9yxlNW5TsNbqdu2Lqjw=";
 
-    nativeBuildInputs = [
-      pkg-config
-      mage
-      fakeGit
-    ];
-
-    buildInputs = [
-      libGL
-      libX11
-      libXcursor
-      libXrandr
-      libXinerama
-      libXi
-      libXxf86vm
-    ];
+    postPatch = ''
+      cp ${./package-lock.json} package-lock.json
+    '';
 
     buildPhase = ''
       runHook preBuild
 
-      # Fixes "mkdir /homeless-shelter: permission denied" - "Error: error compiling magefiles" during build
-      export HOME=$(mktemp -d)
-      mage -d build/magefiles -w . build:full
+      npx esbuild ./web/assets/scripts.js --bundle --minify --outdir=./web/content/
+      npx tailwindcss -i ./web/assets/styles.css -o ./web/content/styles.css --minify
 
       runHook postBuild
-    '';
-
-    ldflags = ["-s" "-w"];
-
-    checkPhase = ''
-      mage -d build/magefiles -w . tests:test
     '';
 
     installPhase = ''
       runHook preInstall
 
-      # Output binary name contains arch name, e.g. go-hass-agent-amd64
-      install -D "$(ls -1 dist/go-hass-agent-*)" $out/bin/go-hass-agent
+      mkdir -p $out
+      cp -r web/content/* $out/
 
       runHook postInstall
     '';
+  };
+in
+  buildGoModule rec {
+    pname = "go-hass-agent";
+    inherit version src;
+
+    vendorHash = "sha256-RMTgvFUgE8u++D9wdBhytpj/NxNAWVVcLjSPi3n5hD8=";
+
+    preBuild = ''
+      cp -r ${frontend}/* web/content/
+    '';
+
+    ldflags = [
+      "-s"
+      "-w"
+      "-X github.com/joshuar/go-hass-agent/config.AppVersion=${version}"
+    ];
 
     meta = {
       description = "A Home Assistant, native app for desktop/laptop devices";
