95ac0fb0eab23f137eedb0fd07e5e8e67269309a
Bazyli Cyran <bazyli@cyran.dev>
2026-04-17 18:43:44 +0200

    feat(joplin-desktop): Add options to disable and configure sync

--- modules/home-manager/programs/joplin-desktop.nix
+++ modules/home-manager/programs/joplin-desktop.nix
@@ -25,13 +25,28 @@
     "org.joplinapp.plugins.admonition.jpl"
   ];
 in {
-  options.my.programs.joplin-desktop.enable = lib.mkEnableOption "joplin-desktop";
+  options.my.programs.joplin-desktop = {
+    enable = lib.mkEnableOption "joplin-desktop";
+    sync = {
+      enable = lib.mkEnableOption "Joplin sync" // {default = true;};
+      path = lib.mkOption {
+        type = lib.types.str;
+        default = "https://joplin.${my.lib.const.domains.intra}";
+        description = "Joplin sync server URL.";
+      };
+      username = lib.mkOption {
+        type = lib.types.str;
+        default = config.my.user.email;
+        description = "Username for Joplin sync server.";
+      };
+    };
+  };
 
   config = lib.mkIf cfg.enable {
     programs.joplin-desktop = {
       enable = true;
       general.editor = lib.getExe pkgs.neovim;
-      sync = {
+      sync = lib.mkIf cfg.sync.enable {
         target = "joplin-server";
         interval = "5m";
       };
@@ -39,78 +54,81 @@ in {
       extraConfig = let
         inherit (config.fonts.fontconfig) defaultFonts;
         editorFont = builtins.elemAt defaultFonts.monospace 0;
-      in {
-        "editor.codeView" = true;
-        "sync.9.path" = "https://joplin.${my.lib.const.domains.intra}";
-        "sync.9.username" = config.my.user.email;
-        locale = "pl_PL";
-        dateFormat = "DD.MM.YYYY";
-        "ocr.enabled" = true;
-        theme = 2;
-        themeAutoDetect = false;
-        layoutButtonSequence = 1;
-        "notes.sortOrder.field" = "user_updated_time";
-        "notes.sortOrder.reverse" = true;
-        "notes.perFieldReverse" = {
-          user_updated_time = true;
-          user_created_time = true;
-          title = false;
-          order = false;
-          todo_due = true;
-          todo_completed = true;
+      in
+        {
+          "editor.codeView" = true;
+          locale = "pl_PL";
+          dateFormat = "DD.MM.YYYY";
+          "ocr.enabled" = true;
+          theme = 2;
+          themeAutoDetect = false;
+          layoutButtonSequence = 1;
+          "notes.sortOrder.field" = "user_updated_time";
+          "notes.sortOrder.reverse" = true;
+          "notes.perFieldReverse" = {
+            user_updated_time = true;
+            user_created_time = true;
+            title = false;
+            order = false;
+            todo_due = true;
+            todo_completed = true;
+          };
+          newTodoFocus = "title";
+          "notes.listRendererId" = "compact";
+          "markdown.plugin.softbreaks" = true;
+          "markdown.plugin.typographer" = true;
+          "markdown.plugin.sub" = true;
+          "markdown.plugin.sup" = true;
+          "markdown.plugin.multitable" = true;
+          showTrayIcon = false;
+          "style.editor.fontSize" = 15;
+          "style.editor.fontFamily" = editorFont;
+          "style.editor.monospaceFontFamily" = editorFont;
+          "ui.layout" = {
+            key = "root";
+            children = [
+              {
+                key = "sideBar";
+                visible = true;
+                width = 210;
+              }
+              {
+                key = "noteList";
+                visible = true;
+                width = 220;
+              }
+              {
+                key = "tempContainer-LesXc856pKyjJUTaxYr7gP";
+                children = [
+                  {
+                    key = "plugin-view-joplin.plugin.note.tabs-note.tabs.panel";
+                    context = {
+                      pluginId = "joplin.plugin.note.tabs";
+                    };
+                    visible = true;
+                    height = 40;
+                  }
+                  {
+                    key = "editor";
+                    visible = true;
+                  }
+                ];
+                visible = true;
+              }
+            ];
+            visible = true;
+          };
+          "clipperServer.autoStart" = true;
+          noteVisiblePanes = ["viewer"];
+          "editor.keyboardMode" = "vim";
+          "editor.spellCheckBeta" = true;
+          "spellChecker.languages" = ["en-US" "pl"];
+          windowContentZoomFactor = 110;
+        }
+        // lib.optionalAttrs cfg.sync.enable {
+          "sync.9.path" = cfg.sync.path;
+          "sync.9.username" = cfg.sync.username;
         };
-        newTodoFocus = "title";
-        "notes.listRendererId" = "compact";
-        "markdown.plugin.softbreaks" = true;
-        "markdown.plugin.typographer" = true;
-        "markdown.plugin.sub" = true;
-        "markdown.plugin.sup" = true;
-        "markdown.plugin.multitable" = true;
-        showTrayIcon = false;
-        "style.editor.fontSize" = 15;
-        "style.editor.fontFamily" = editorFont;
-        "style.editor.monospaceFontFamily" = editorFont;
-        "ui.layout" = {
-          key = "root";
-          children = [
-            {
-              key = "sideBar";
-              visible = true;
-              width = 210;
-            }
-            {
-              key = "noteList";
-              visible = true;
-              width = 220;
-            }
-            {
-              key = "tempContainer-LesXc856pKyjJUTaxYr7gP";
-              children = [
-                {
-                  key = "plugin-view-joplin.plugin.note.tabs-note.tabs.panel";
-                  context = {
-                    pluginId = "joplin.plugin.note.tabs";
-                  };
-                  visible = true;
-                  height = 40;
-                }
-                {
-                  key = "editor";
-                  visible = true;
-                }
-              ];
-              visible = true;
-            }
-          ];
-          visible = true;
-        };
-        "clipperServer.autoStart" = true;
-        noteVisiblePanes = ["viewer"];
-        "editor.keyboardMode" = "vim";
-        "editor.spellCheckBeta" = true;
-        "spellChecker.languages" = ["en-US" "pl"];
-        windowContentZoomFactor = 110;
-      };
     };
 
     xdg.configFile = let
