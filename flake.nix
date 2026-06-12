6c76e3620cfa6fc26c3c4ba5bd75f085ca3ac8eb
Bazyli Cyran <bazyli@cyran.dev>
2026-06-12 20:02:13 +0200

    chore(flake): update the lockfile

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia-shell?ref=legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     timewall = {
